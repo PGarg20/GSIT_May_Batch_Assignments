282e088eff7aab7667d863e6fd3c161b583da4a6
preeti garg <preeti.ec15@gmail.com>
2026-07-01 19:48:06 +0530

    daily assignment added

---
+++ Daily Assignment/Assignmen1_30_June.java
@@ -0,0 +1,30 @@
+package day4;
+
+public class Assignmen1_30_June {
+	
+	public static void main(String[] args) {
+		
+		char ch='A';
+		int i=0;
+		
+		while (ch<='Z')
+		{
+			
+			i++;
+			
+			if(i%2!=0)
+			{
+				//System.out.println(i);
+
+			System.out.print(ch+ " ");
+
+			}
+			
+			ch++;
+			
+			
+			
+		}
+	}
+
+}
---
+++ Daily Assignment/Assignment2_30_June.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class Assignment2_30_June {
+	
+public static void main(String[] args) {
+		
+		char ch='A';
+		int value=(int)ch;
+		
+		while (ch<='Z')
+		{
+			System.out.println(ch+ " = " +value);
+			ch++;
+			value++;
+		}
+	}
+
+}
